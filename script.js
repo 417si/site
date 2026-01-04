6deb316dbfba508aab99433d07256d6d3031b25d
417si <417kota.2@gmail.com>
2026-01-04 15:17:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,13 @@ const noNewlineCount = document.getElementById('no-newline-count');
 const noSpaceCount = document.getElementById('no-space-count');
 
 textInput.addEventListener('input', () => {
-    const val = textInput.value;
+	const val = textInput.value;
 
-    // 1. 全文字数
-    charCount.textContent = val.length;
+	charCount.textContent = val.length;
 
-    // 2. 改行を除く文字数
-    const noNewline = val.replace(/\n/g, '');
-    noNewlineCount.textContent = noNewline.length;
+	const noNewline = val.replace(/\n/g, '');
+	noNewlineCount.textContent = noNewline.length;
 
-    // 3. 空白・改行を除く文字数
-    const noSpace = val.replace(/\s/g, '');
-    noSpaceCount.textContent = noSpace.length;
+	const noSpace = val.replace(/\s/g, '');
+	noSpaceCount.textContent = noSpace.length;
 });
